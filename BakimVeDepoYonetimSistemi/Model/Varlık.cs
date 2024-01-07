0130291a378ea59829f3386154153aae6d05de46
İlkay Durğun <101176983+ilkydrgun@users.noreply.github.com>
2024-01-07 23:25:49 +0300

    Update Varlık.cs

--- BakimVeDepoYonetimSistemi/Model/Varlık.cs
+++ BakimVeDepoYonetimSistemi/Model/Varlık.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace Deneme.Class
+
+﻿namespace BakimVeDepoYonetimSistemi.Model
 {
     public class Varlık
     {
